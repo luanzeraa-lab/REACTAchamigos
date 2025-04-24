3d85a79d81603be191721f694e437a4147387200
Luan Silva de Picoli <luansilva_2@hotmail.com>
2025-04-23 22:46:33 -0300

    Adicionando os arquivos do projeto React

---
+++ my-app/src/app/informacoes/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+
+import Nav2 from "@/components/Nav2";
+import Footer from "@/components/Footer";
+
+
+const Informacoes =()=>{
+
+    return(
+        <>
+            <Nav2/>
+            
+            
+                
+            
+            <Footer/>
+        </>
+
+    )
+}
+export default Informacoes;
---
+++ my-app/src/app/parceiros/page.tsx
@@ -0,0 +1,28 @@
+
+"use client";
+ 
+import Image from "next/image";
+import { Container} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Nav2 from "@/components/Nav2";
+import Footer from "@/components/Footer";
+ 
+const Parceiros = () => {
+  return (
+    <>
+ 
+      <Container fluid className="bg-[#ffeccf]">
+        <Nav2/>
+ 
+        
+
+        <Footer/>
+      </Container>
+ 
+    
+    </>
+  );
+};
+ 
+export default Parceiros;
+ 
