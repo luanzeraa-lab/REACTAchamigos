fe8fad2daf461a71171e57a43dc376e6d49b2234
Luan Silva de Picoli <luansilva_2@hotmail.com>
2025-06-15 12:01:52 -0300

    implementação da validação no login

--- my-app/src/components/Logar.tsx
+++ my-app/src/components/Logar.tsx
@@ -1,33 +1,57 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Form from 'react-bootstrap/Form';
+
 import { useRouter } from 'next/navigation';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from "yup";
+
 const Logar =()=>{
+
+    const validation = Yup.object({
+        emailcpf: Yup.string().required("Campo obrigatório"),
+        password: Yup.string().required("Campo obrigatório"),
+    })
 
     const router = useRouter();
 
     return (
         <>
-            
-            <div className='mt-4 d-flex justify-center'>
-                <div className="text-center col-4">
-                    <p className='font-bold text-2xl'>Login</p>
-                    <Form.Label htmlFor="login">Email ou CPF</Form.Label>
-                    <Form.Control
-                        type="text"
-                        id="login"
-                    />
-                    <Form.Label htmlFor="senha">Senha</Form.Label>
-                    <Form.Control
-                        type="password"
-                        id="senha"
-                        aria-describedby="passwordHelpBlock"
-                    />
-                    <input className="border-2 rounded-full bg-[#ff8110] w-20 mt-2" type="button" value="Entrar"
-                    onClick={()=>{
-                        router.push("/CadastroAnimais")}}/>
-                </div>
-            </div>
-  
+            <Formik
+                initialValues={{emailcpf:"", password:""}}
+                validationSchema={validation}
+                onSubmit={(valores)=>{
+                    console.log(valores.emailcpf);
+                    console.log(valores.password);
+                    router.push("/CadastroAnimais")
+                }}>
+                <Form>
+                    <div className='mt-4 d-flex justify-center'>
+                        <div className="text-center col-4">
+                            <p className='font-bold text-2xl'>Login</p>
+                            
+                            <label htmlFor="emailcpf">Email ou CPF</label>
+                            <Field
+                            type="text"
+                            name="emailcpf"
+                            className="form-control"
+                            id="emailcpf"
+                             />
+                             <ErrorMessage name='emailcpf' component="div"/>
+                            
+                             <label htmlFor="password" className="mt-2">Senha</label>
+                        <Field
+                            type="password"
+                            name="password"
+                            className="form-control"
+                            id="password"
+                        />
+                        <ErrorMessage name="password" component="div" />
+                           
+                           <button className="border-2 rounded-full bg-[#ff8110] w-20 mt-2" type="submit">Entrar</button>
+                            
+                        </div>
+                    </div>
+                </Form>    
+            </Formik>                
         </>
     )
 }
